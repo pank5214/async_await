c135c4d0ca9395e10ba00ee37db65273c1abc413
pank5214 <pankajkumar5214@gmail.com>
2023-09-21 17:11:47 +0530

    theory

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 console.log("Hello world!");
 
+// async returns a promise => Promise is an object which represents the eventual completion or failure of an async operation.
+
+// await can only use inside an async function, if use somewhereElse it's give an Error.
+
 const API_URL = "https://api.github.com/users/pank5214";
 const promiseHandle = async () => {
-  try {
-    const data = await fetch(API_URL);
+  const data = await fetch(API_URL);
 
-    const json = await data.json();
-    console.log(json);
-  } catch (err) {
-    console.log("404 : Not Found", err);
-  }
+  // fetch() => Response => Response.json() => jsonValue
+
+  const jsonValue = await data.json();
+  console.log(jsonValue);
 };
 
-promiseHandle();
+
+// another way of handling error instead of using "try{} catch{}" method.
+promiseHandle().catch((err) => console.log(err));
